9e9ebdbf4cd8503082e76fc61ebb300925536d16
Weiming Zhai <wmzhai@gmail.com>
2016-02-17 11:41:30 +0800

    fix scripts

--- content/accounts1.md
+++ content/accounts1.md
@@ -1,1 +1,5 @@
 # 账户系统
+
+Meteor内置了一个可以直接使用的账户系统及其界面，并基于它实现了多个第三方账号的集成。
+
+账户系统的核心内容在**accounts-base**包里面实现，一般的应用程序通过添加**accounts-password**、**accounts-weibo** 等来自动引用**accounts-base**。
--- resources/deploy.sh
+++ resources/deploy.sh
@@ -6,7 +6,7 @@ rm -rf out || exit 0;
 mkdir out;
 
 # run our compile script, discussed above
-npm run build
+npm run b
 
 # go to the out directory and create a *new* Git repo
 cd out
